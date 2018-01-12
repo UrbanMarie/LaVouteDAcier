38e91b4c493811f5bea445ed0a017daffa365652
UrbanMarie <hippchz@gmail.com>
2018-01-12 11:18:47 +0100

    Add Ligthbox and fixed the hover of the gallery

---
+++ page/gallery.php
@@ -0,0 +1,106 @@
+<?php include 'header.html';?>
+<?php include 'nav.html';?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-1"></div>   <!-- Mettre Offset -->
+
+         <div class="col-md-9  gallery back-white rounded pad-small marg-top-medium">
+
+            <div class="row">
+                    <div class="col-md-4 crop-img-small crop-img gallery-contain">
+                        <div class="gall">
+                            <a href="../001.JPG" data-lightbox="example-set" data-title="."><img href="projet_temp.php" src="../001.JPG" alt="header soudure" class="gallery-img"></a>
+                        </div>
+
+                        <div class="middle-text">
+                            <p class="text text-center impact"> AGRANDIR L'IMAGE </p>
+                            <p class="text-second text-center"> <a href="projet_temp.php"> Découvrir le projet </a> </p>
+                        </div>
+                    </div>
+
+                <div class="col-md-8 crop-img-medium crop-img gallery-contain">
+                    <div class="gall">
+                        <a href="../01.JPG" data-lightbox="example-set" data-title="."><img src="../01.jpg" alt="header soudure" class="gallery-img"></a>
+                    </div>
+                    <div class="middle-text">
+                        <p class="text impact text-center"> AGRANDIR L'IMAGE </p>
+                        <p class="text-second text-center"> <a href="projet_temp.php"> Découvrir le projet </a> </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4 crop-img-small crop-img gallery-contain">
+                    <div class="gall">
+                        <a href="../02.JPG" data-lightbox="example-set" data-title="."><img src="../02.jpg" alt="header soudure" class="gallery-img"></a>
+                    </div>
+                    <div class="middle-text">
+                        <p class="text impact text-center"> AGRANDIR L'IMAGE </p>
+                        <p class="text-second text-center"> <a href="projet_temp.php"> Découvrir le projet </a> </p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 crop-img-small crop-img gallery-contain">
+                   <div class="gall">
+                       <a href="../003.JPG" data-lightbox="example-set" data-title="."><img src="../003.JPG" alt="header soudure" class="gallery-img"></a>
+                   </div>
+                    <div class="middle-text">
+                        <p class="text impact text-center"> AGRANDIR L'IMAGE </p>
+                        <p class="text-second text-center"> <a href="projet_temp.php"> Découvrir le projet </a> </p>
+                    </div>
+
+                </div>
+
+
+                <div class="col-md-4 crop-img-small crop-img gallery-contain">
+                    <div class="gall">
+                        <a href="../004.JPG" data-lightbox="example-set" data-title="."><img src="../004.JPG" alt="header soudure" class="gallery-img"></a>
+                    </div>
+                    <div class="middle-text">
+                        <p class="text impact text-center"> AGRANDIR L'IMAGE </p>
+                        <p class="text-second text-center"> <a href="projet_temp.php"> Découvrir le projet </a> </p>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-8 crop-img-medium crop-img">
+                    <a href="../03.jpg" data-lightbox="example-set" data-title="."><img src="../03.jpg" alt="header soudure"></a>
+                </div>
+                <div class="col-md-4 crop-img-small crop-img">
+                    <a href="../05.jpg" data-lightbox="example-set" data-title="."><img src="../05.jpg" alt="header soudure"></a>
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4 crop-img-small crop-img">
+                    <a href="../home011.png" data-lightbox="example-set" data-title="."><img src="../home011.png" alt="header soudure"></a>
+                </div>
+                <div class="col-md-4 crop-img-small crop-img">
+                    <a href="../02.jpg" data-lightbox="example-set" data-title="."><img src="../02.jpg" alt="header soudure"></a>
+                </div>
+                <div class="col-md-4 crop-img-small crop-img">
+                    <a href="../soudure.jpg" data-lightbox="example-set" data-title="."><img src="../soudure.jpg" alt="header soudure"></a>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-2">
+            <div class="relative">
+                <img class="backElement" id="backlist-element-vert" src="../list-gallery-decor.svg" alt="#">
+                        <ul class="pull-right z3 list-menu">
+                            <li><a> Verrière</a></li>
+                            <li><a> Portail</a></li>
+                            <li><a> Rampe</a></li>
+                            <li><a> Mobilier</a></li>
+                            <li><a> Escalier</a></li>
+                            <li><a> Fenêtres</a></li>
+                            <li><a> Grilles</a></li>
+                        </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'footer.html';?>
