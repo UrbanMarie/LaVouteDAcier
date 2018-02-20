84f122f1f3a4a1d9be66fc2bef971b5d62caaeb4
UrbanMarie <hippchz@gmail.com>
2018-02-20 18:30:23 +0100

    Page of the website

---
+++ page/old/gallery.php
@@ -0,0 +1,122 @@
+<?php include 'header.html';?>
+<?php include 'nav.html';?>
+    <div class="container-fluid relative">
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="relative menu-alternatif">
+                    <ul id="alternative-list">
+                        <li class="menu"><a href="gallery.php"> Escalier</a></li>
+                        <li class="menu"><a href="gall_verriere.php"> Verrière</a></li>
+                        <li class="menu"><a href="porte.php"> Portail</a></li>
+                        <li class="menu"><a href="mobilier.php"> Mobilier</a></li>
+                        <li class="menu"><a href="gardes.php"> GardeCorps</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-1"></div>
+                <div class="col-md-9 back-white rounded border-marron pad-small marg-top-medium">
+                    <section>
+                        <div class="rowGal">
+
+                            <div class="column">
+                                <div class="gallery-contain">
+                                    <div class="gall">
+                                        <a href="../img/gallerie/Escalier/projet1/004.JPG" data-lightbox="example-set" data-title="."><img href="projet_temp.php" src="../img/gallerie/Escalier/projet1/004.JPG" alt=" Escalier" class="gallery-img"></a>
+                                    </div>
+                                    <div class="middle-text">
+                                        <p class="text text-center impact"> AGRANDIR L'IMAGE </p>
+                                    </div>
+                                 </div>
+                                <div class="gallery-contain">
+                                    <div class="gall">
+                                        <a href="../img/gallerie/Escalier/projet4/04.jpg" data-lightbox="example-set" data-title="."><img href="projet_temp.php" src="../img/gallerie/Escalier/projet4/04.jpg" alt=" Escalier" class="gallery-img"></a>
+                                    </div>
+                                    <div class="middle-text">
+                                        <p class="text text-center impact"> AGRANDIR L'IMAGE </p>
+                                    </div>
+                                </div>
+                                </div>
+
+                            <div class="column">
+                                <div class="gallery-contain">
+                                    <div class="gall">
+                                        <a href="../img/gallerie/Escalier/projet3/13.jpg" data-lightbox="example-set" data-title="."><img href="projet_temp.php" src="../img/gallerie/Escalier/projet3/13.jpg" alt=" Escalier" class="gallery-img"></a>
+                                    </div>
+                                    <div class="middle-text">
+                                        <p class="text text-center impact"> AGRANDIR L'IMAGE </p>
+                                    </div>
+                                </div>
+                                <div class="gallery-contain">
+                                    <div class="gall">
+                                        <a href="../img/gallerie/Escalier/projet2/12.jpg" data-lightbox="example-set" data-title="."><img href="projet_temp.php" src="../img/gallerie/Escalier/projet2/12.jpg" alt=" Escalier" class="gallery-img"></a>
+                                    </div>
+                                    <div class="middle-text">
+                                        <p class="text text-center impact"> AGRANDIR L'IMAGE </p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="column">
+                                <div class="gallery-contain">
+                                    <div class="gall">
+                                        <a href="../img/gallerie/Escalier/projet6/06.jpg" data-lightbox="example-set" data-title="."><img href="projet_temp.php" src="../img/gallerie/Escalier/projet6/06.jpg" alt=" Escalier" class="gallery-img"></a>
+                                    </div>
+                                    <div class="middle-text">
+                                        <p class="text text-center impact"> AGRANDIR L'IMAGE </p>
+                                    </div>
+                                </div>
+                                <div class="gallery-contain">
+                                    <div class="gall">
+                                        <a href="../img/gallerie/Escalier/projet5/08.jpg" data-lightbox="example-set" data-title="."><img href="projet_temp.php" src="../img/gallerie/Escalier/projet5/08.jpg" alt=" Escalier" class="gallery-img"></a>
+                                    </div>
+                                    <div class="middle-text">
+                                        <p class="text text-center impact"> AGRANDIR L'IMAGE </p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="column">
+                                <div class="gallery-contain">
+                                    <div class="gall">
+                                        <a href="../img/gallerie/Escalier/projet7/16.jpg" data-lightbox="example-set" data-title="."><img href="projet_temp.php" src="../img/gallerie/Escalier/projet7/16.jpg" alt=" Escalier" class="gallery-img"></a>
+                                    </div>
+                                    <div class="middle-text">
+                                        <p class="text text-center impact"> AGRANDIR L'IMAGE </p>
+                                    </div>
+                                </div>
+                                <div class="gallery-contain">
+                                    <div class="gall">
+                                        <a href="../img/gallerie/Escalier/projet8/15.jpeg" data-lightbox="example-set" data-title="."><img href="projet_temp.php" src="../img/gallerie/Escalier/projet8/15.jpeg" alt=" Escalier" class="gallery-img"></a>
+                                    </div>
+                                    <div class="middle-text">
+                                        <p class="text text-center impact"> AGRANDIR L'IMAGE </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+
+            <div class="col-md-2" id="list-gall">
+
+                <aside class="list-menu">
+                    <div class="relative">
+                        <ul class="list-menu">
+                            <li><a href="gallery.php"> Escalier</a></li>
+                            <li><a href="gall_verriere.php"> Verrière</a></li>
+                            <li><a href="porte.php"> Portail</a></li>
+                            <li><a href="mobilier.php"> Mobilier</a></li>
+                            <li><a href="gardes.php"> GardeCorps</a></li>
+                        </ul>
+                    </div>
+                </aside>
+            </div>
+        </div>
+
+<?php include 'footer.html';?>
---
+++ page/projetwip.php
@@ -0,0 +1,75 @@
+<?php include 'header.html';?>
+<?php include 'nav.html';?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10 rounded pad-small marg-top-medium">
+
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-5 pola-contain">
+                        <div class="polaroid">
+                            <div class="crop-pola crop-polaroid-img">
+                                <a href="projet_temp.php"><img src="../001.JPG" alt="#" class="image-pola"/></a>
+                            </div>
+                            <div class="middle-text">
+                                <p class="text text-center impact"> DÉCOUVRIR LE PROJET </p>
+                                <p class="text-second text-center">  Réalisation pour une enseigne de prêt a porter </p>
+                            </div>
+                        </div>
+                        <p class="pad-top-tiny small-cap ">Chazelle sur Lyon, Étagère </p>
+                    </div>
+
+                    <div class="col-md-5 pola-contain">
+                        <div class="polaroid">
+                            <div class="crop-pola crop-polaroid-img">
+                                <a href="projet_temp.php"><img src="../03.jpg" alt="#" class="image-pola"/></a>
+                            </div>
+                            <div class="middle-text">
+                                <p class="text text-center impact"> DÉCOUVRIR LE PROJET </p>
+                                <p class="text-second text-center">  Réalisation pour un particulier </p>
+                            </div>
+                        </div>
+                        <p class="pad-top-tiny small-cap ">Lyon, Mezzanine </p>
+                    </div>
+                    <div class="col-md-1"></div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-5 pola-contain">
+                        <div class="polaroid">
+                            <div class="crop-pola crop-polaroid-img">
+                                <a href="projet_temp.php"><img src="../02.jpg" alt="#" class="image-pola"/></a>
+                            </div>
+                            <div class="middle-text">
+                                <p class="text text-center impact"> DÉCOUVRIR LE PROJET </p>
+                                <p class="text-second text-center">  Réalisation pour un particulier </p>
+                            </div>
+                        </div>
+                        <p class="pad-top-tiny small-cap ">Saint-Étienne, Portail </p>
+                    </div>
+
+                    <div class="col-md-5 pola-contain">
+                        <div class="polaroid">
+                            <div class="crop-pola crop-polaroid-img">
+                                <a href="projet_temp.php"><img src="../01.jpg" alt="#" class="image-pola"/></a>
+                            </div>
+                            <div class="middle-text">
+                                <p class="text text-center impact"> DÉCOUVRIR LE PROJET </p>
+                                <p class="text-second text-center">  Réalisation pour une mairie </p>
+                            </div>
+                        </div>
+                        <p class="pad-top-tiny small-cap ">Chazelle sur Lyon, Rampe </p>
+                    </div>
+                    <div class="col-md-1"></div>
+                </div>
+                <div class="col-md-1"></div>
+            </div>
+        </div>
+
+        <div class="col-md-1">
+        </div>
+    </div>
+
+<?php include 'footer.html';?>
